91e50819ccb8ca2646230887d68c0554fa23a96d
Yurii <shadowjura82@gmail.com>
2023-04-05 16:03:19 +0200

    task1 has been completed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+    }
+
+    public static void task1() {
+        int investitionValue = 15000;
+        int summCount = 0;
+        int monthCount = 0;
+        System.out.println("Задание 1");
+        while (summCount < 2_459_000) {
+            summCount += investitionValue;
+            System.out.println("Месяц " + monthCount + ", сумма накоплений равна " + summCount + " рублей");
+            monthCount++;
+        }
+        System.out.println();
     }
 }
